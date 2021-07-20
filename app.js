c0abb6af0dadef2969b90a998ffe0afa679064db
kimja01 <kimja7045@naver.com>
2021-07-20 21:43:29 +0900

    removeAll at ts

--- app.js
+++ app.js
@@ -1,30 +1,15 @@
-type Store = {
-    currentPage: number
-    feeds: NewsFeed[]
-}
-
-type NewsFeed = {
-    id: number
-    comments_count: number
-    url: string
-    time_age: string
-    points: number
-    title: string
-    read?: boolean
-}
-
-const container: HTMLElement | null = document.getElementById('root')
-const ajax: XMLHttpRequest = new XMLHttpRequest()
+const container = document.getElementById('root')
+const ajax = new XMLHttpRequest()
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'
-// const store = {
-//     currentPage: 1,
-//     feeds: [],
-// }
-const store: Store = {
+const store = {
     currentPage: 1,
     feeds: [],
 }
+// const store: Store = {
+//     currentPage: 1,
+//     feeds: [],
+// }
 
 function getData(url) {
     ajax.open('GET', url, false)
@@ -42,7 +27,7 @@ function makeFeeds(feeds) {
 }
 
 function newsFeed() {
-    let newsFeed: NewsFeed[] = store.feeds
+    let newsFeed = store.feeds
     const newsList = []
     let templace = `
 
@@ -58,7 +43,12 @@ console.log(newsFeed)
 
 for (let i = 0; i < newsFeed.length; i++) {
     const li = document.createElement('li')
+    const a = document.createElement('a')
+
+    a.href = '#'
     li.innerHTML = newsFeed[i].title
+
+    li.appendChild(a)
     ul.appendChild(li)
 }
 // let data = '<ul>'
