468d20572d03b2f8946e502aec314f6995fbf69e
kimja01 <kimja7045@naver.com>
2021-08-21 18:25:12 +0900

    Update refactoring at inheritance

--- app.ts
+++ app.ts
@@ -42,6 +42,34 @@ const store: Store = {
     feeds: [],
 }
 
+class Api {
+    url: string
+    ajax: XMLHttpRequest
+    constructor(url: string) {
+        this.url = url
+        this.ajax = new XMLHttpRequest()
+    }
+
+    protected getRequest<AjaxResponse>(): AjaxResponse {
+        this.ajax.open('GET', this.url, false)
+        this.ajax.send()
+
+        return JSON.parse(this.ajax.response)
+    }
+}
+
+class NewsFeedApi extends Api {
+    getData(): NewsFeed[] {
+        return this.getRequest<NewsFeed[]>()
+    }
+}
+
+class NewsDetailApi extends Api {
+    getData(): NewsDetail {
+        return this.getRequest<NewsDetail>()
+    }
+}
+
 function getData<AjaxResponse>(url: string): AjaxResponse {
     ajax.open('GET', url, false)
     ajax.send()
@@ -65,6 +93,7 @@ function updateView(html: string): void {
     }
 }
 function newsFeed(): void {
+    const api = new NewsFeedApi(NEWS_URL)
     let newsFeed: NewsFeed[] = store.feeds
     const newsList = []
     let template = `
@@ -93,7 +122,7 @@ function newsFeed(): void {
     `
 
     if (newsFeed.length === 0) {
-        newsFeed = store.feeds = makeFeeds(getData<NewsFeed[]>(NEWS_URL))
+        newsFeed = store.feeds = makeFeeds(api.getData())
     }
 
     for (
@@ -141,7 +170,8 @@ function newsFeed(): void {
 
 function newsDetail(): void {
     const id = location.hash.substr(7)
-    const newsContent = getData<NewsDetail>(CONTENT_URL.replace('@id', id))
+    const api = new NewsDetailApi(CONTENT_URL.replace('@id', id))
+    const newsContent = api.getData()
     let template = `
     <div class="bg-gray-600 min-h-screen pb-8">
     <div class="bg-white text-xl">
