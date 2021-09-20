9f247546d10c13c9fd9edb7541f6eadf0e963537
kimja01 <kimja7045@naver.com>
2021-09-20 18:28:05 +0900

    Update xhr to fetch at 4_9

--- src/core/api.ts
+++ src/core/api.ts
@@ -18,20 +18,31 @@ function applyApiMixins(targetClass: any, baseClasses: any[]): void {
     })
 }
 export default class Api {
-    ajax: XMLHttpRequest
+    xhr: XMLHttpRequest
     url: string
 
     constructor(url: string) {
-        this.ajax = new XMLHttpRequest()
+        this.xhr = new XMLHttpRequest()
         this.url = url
     }
 
-    getRequest<AjaxResponse>(cb: (data: AjaxResponse) => void): void {
-        this.ajax.open('GET', this.url)
-        this.ajax.addEventListener('load', () => {
-            cb(JSON.parse(this.ajax.response) as AjaxResponse)
+    getRequestWithXHR<AjaxResponse>(cb: (data: AjaxResponse) => void): void {
+        this.xhr.open('GET', this.url)
+        this.xhr.addEventListener('load', () => {
+            cb(JSON.parse(this.xhr.response) as AjaxResponse)
         })
-        this.ajax.send()
+        this.xhr.send()
+    }
+
+    getRequestWithPromise<AjaxResponse>(
+        cb: (data: AjaxResponse) => void
+    ): void {
+        fetch(this.url)
+            .then((response) => response.json())
+            .then(cb)
+            .catch(() => {
+                console.error('데이터를 불러오지 못했습니다.')
+            })
     }
 }
 
@@ -40,8 +51,12 @@ export class NewsFeedApi extends Api {
         super(url)
     }
 
-    getData(cb: (data: NewsFeed[]) => void): void {
-        return this.getRequest<NewsFeed[]>(cb)
+    getDataWithXHR(cb: (data: NewsFeed[]) => void): void {
+        return this.getRequestWithXHR<NewsFeed[]>(cb)
+        // return this.getRequest<NewsFeed[]>(NEWS_URL)
+    }
+    getDataWithPromise(cb: (data: NewsFeed[]) => void): void {
+        return this.getRequestWithPromise<NewsFeed[]>(cb)
         // return this.getRequest<NewsFeed[]>(NEWS_URL)
     }
 }
@@ -51,9 +66,13 @@ export class NewsDetailApi extends Api {
         super(url)
     }
 
-    getData(cb: (data: NewsDetail) => void): void {
-        return this.getRequest<NewsDetail>(cb)
+    getDataWithXHR(cb: (data: NewsDetail) => void): void {
+        return this.getRequestWithXHR<NewsDetail>(cb)
         // return this.getRequest<NewsDetail>(CONTENT_URL.replace('@id', id))
+    }
+    getDataWithPromise(cb: (data: NewsDetail) => void): void {
+        return this.getRequestWithPromise<NewsDetail>(cb)
+        // return this.getRequest<NewsFeed[]>(NEWS_URL)
     }
 }
 
